428cba278de6972fc64db1381a4db8e9fca923d1
KYLN24 <1296845690@qq.com>
2024-03-26 22:08:21 +0800

    change json filename for danxi swift

--- dx_static_convert_toml_to_json.py
+++ dx_static_convert_toml_to_json.py
@@ -6,7 +6,7 @@ print(">>>> BEGIN: Convert toml to json >>>>")
 
 try:
     with open("./tmp_wait_for_json_editor.toml", "r", encoding="utf8") as f_toml:
-        with open("./tmp_wait_for_json_editor.json", "w", encoding="utf8") as f_json:
+        with open("./swift.json", "w", encoding="utf8") as f_json:
             json.dump(toml.load(f_toml), f_json, ensure_ascii=False)
     print("SUCCESS")
 except:
