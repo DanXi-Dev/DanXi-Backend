713976c738401e0c75446b542b37eafae222a133
KYLN24 <1296845690@qq.com>
2024-07-03 17:10:16 +0800

    feat: crash when fail to parse

--- dx_static_convert_toml_to_json.py
+++ dx_static_convert_toml_to_json.py
@@ -9,7 +9,8 @@ try:
         with open("./public/swift.json", "w", encoding="utf8") as f_json:
             json.dump(toml.load(f_toml), f_json, ensure_ascii=False)
     print("SUCCESS")
-except:
+except Exception as e:
     print("ERROR")
+    raise from e
 
 print("<<<< END: Convert toml to json <<<<")
